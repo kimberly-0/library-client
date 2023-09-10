11d472513d3fcdf77e4f129bb560ec7e3b2e9b35
Kimberly D <kim.14420@outlook.com>
2023-09-10 07:16:21 +0100

    create member form

---
+++ src/components/memberForm/MemberForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const MemberForm = ({ handleSubmit, initialData = {
+    firstName: '',
+    surname: '',
+    profileImage: '',
+}}) => {
+
+    const [member, setMember] = useState(initialData);
+
+    const handleChange = (fields) => {
+        setMember(prev => {
+            return { ...prev, ...fields }
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        handleSubmit({ member }).catch(err => {
+            console.log(err);
+        });   
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+
+            <div className="form-field">
+                <label htmlFor="firstName">First name</label>
+                <input
+                    type='firstName'
+                    id='firstName'
+                    name='firstName'
+                    value={member.firstName}
+                    onChange={e => {handleChange({firstName: e.target.value})}}
+                    placeholder='firstName'
+                />
+            </div>
+
+            <div className="form-field">
+                <label htmlFor="surname">Surname</label>
+                <input
+                    type='surname'
+                    id='surname'
+                    name='surname'
+                    value={member.surname}
+                    onChange={e => {handleChange({surname: e.target.value})}}
+                    placeholder='surname'
+                />
+            </div>
+
+            <button type='submit'>Save</button>
+        </form>
+    )
+}
+
+export default MemberForm;
