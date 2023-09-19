5ab12a81242c6bfc1f61b9be54778f554f770e7a
Kimberly D <kim.14420@outlook.com>
2023-09-19 11:00:25 +0200

    add book cover image and styling classnames

--- src/pages/books/Books.js
+++ src/pages/books/Books.js
@@ -1,3 +1,4 @@
+import "./Books.css";
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { deleteBook } from '../../api/book';
@@ -22,33 +23,40 @@ const Books = ({ books, deleteLocalBook }) => {
 
     return (
         <>
-            <Link to={'/books/add'}>Add new book</Link>
+            <Link to={'/books/add'} className="add-new-button">Add new book</Link>
 
             {books?.length > 0 ? (
                 <table>
                     <thead>
                         <tr>
+                            <th>Cover</th>
                             <th>Title</th>
                             <th>Author</th>
                             <th>On loan</th>
                             <th>Borrower</th>
                             <th>Issue/Return</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {books.map(book => (
                             <tr key={book.id}>
+                                <td>{book.coverImageURL ? 
+                                    <img className="bookCoverImg" src={book.coverImageURL} alt={book.title} /> 
+                                    : 
+                                    <p className="noImg">No image available</p>}
+                                </td>
                                 <td>{book.title ? book.title : ""}</td>
                                 <td>{book.authorFirstName ? book.authorFirstName : ""} {book.authorSurname ? book.authorSurname : ""}</td>
                                 <td>{book.onLoan ? "yes" : "no"}</td>
                                 <td>{book.borrower?.firstName ? `${book.borrower.firstName}` : ""} {book.borrower?.surname ? `${book.borrower.surname}` : ""}</td>
                                 <td>{book.onLoan ? 
-                                    <Link to={`/books/${book.id}/return/${book.borrower.id}`}>Return</Link> 
+                                    <Link to={`/books/${book.id}/return/${book.borrower.id}`} className="action-button">Return</Link> 
                                     : 
-                                    <Link to={`/books/${book.id}/issue`}>Issue</Link>
+                                    <Link to={`/books/${book.id}/issue`} className="action-button">Issue</Link>
                                 }</td>
-                                <td><Link to={`/books/${book.id}/edit`} >Edit</Link></td>
-                                <td><button onClick={() => handleDelete(book)} >Delete</button></td>
+                                <td><Link to={`/books/${book.id}/edit`} className="action-button">Edit</Link></td>
+                                <td><button onClick={() => handleDelete(book)} className="action-button delete-button">Delete</button></td>
                             </tr>
                         ))}
                     </tbody>
